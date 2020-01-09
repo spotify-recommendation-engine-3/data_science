bae9ba7f657ce2edeb0614505d0728d8c6763117
Tclack88 <39845330+Tclack88@users.noreply.github.com>
2020-01-09 11:48:21 -0800

    Update app.py
    - removed "test_song" variable (vestigial remnant from initial tests)
    - reset indices on output from their sql index to 0-9

--- spotify-flask-model/app.py
+++ spotify-flask-model/app.py
@@ -10,8 +10,6 @@ songs_df = pd.read_csv(infile)
 y = songs_df[songs_df.columns[:3]]
 X = songs_df[songs_df.columns[3:]]
 
-test_song = {"artist_name":"YG","track_id":"2RM4jf1Xa9zPgMGRDiht8O","track_name":"Big Bank feat. 2 Chainz, Big Sean, Nicki Minaj","acousticness":0.00582,"danceability":0.743,"duration_ms":238373,"energy":0.339,"instrumentalness":0.0,"key":1,"liveness":0.0812,"loudness":-7.678,"mode":1,"speechiness":0.409,"tempo":203.927,"time_signature":4,"valence":0.118,"popularity":15}
-
 def preprocess(df):
     """ normalizes pandas df.
     Removes unecessary columns """
@@ -31,7 +29,7 @@ def suggest_songs(source_song, model):
     """ Preprecesses source song, use it to make suggestions from the database """
     source_song = preprocess(source_song)
     recommendations = model.kneighbors(source_song)[1][0][1:] #remove 1st result (source)
-    recommendations_dict = y.iloc[recommendations].T.to_dict()
+    recommendations_dict = y.iloc[recommendations].reset_index(drop=True).T.to_dict()
     return recommendations_dict
 
 my_model = create_model(preprocess(X))
